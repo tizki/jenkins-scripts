0f8b173f1e163d08565a96a3bfb38ad33b6a5342
Tidhar Klein Orbach <tidhar.o@taboola.com>
2018-01-14 16:59:52 +0200

    printBuildsPerNodeByTimestamp

---
+++ console-scripts/printBuildsPerNodeByTimestamp.groovy
@@ -0,0 +1,17 @@
+def jenkins = Jenkins.instance
+def jobName = "myJob"
+def bldNum = 123
+def job = jenkins.getItem(myJob)
+def bld = job.getBuildByNumber(bldNum)
+
+long hourIInMillis = 3600000
+long halfHourIInMillis = 1800000
+long quarterHourIInMillis = 900000
+long endTime =  bld.getTimeInMillis()
+long startTime = endTime-halfHourIInMillis
+jenkins.getComputers().each{
+    def node = it.getNode()
+    println "******* ${node.getDisplayName()} ********"
+    println job.getBuilds().byTimestamp(startTime,endTime).node(node)
+}
+
