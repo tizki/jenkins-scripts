7ff92879fa6add2b49dbdc5727bdfac0faca3bb9
Tidhar Klein Orbach <tidhar.o@taboola.com>
2018-09-20 17:11:08 +0300

    fixed disable timer trigger, added disable SCM trigger

--- console-scripts/disableTimerTriggeredJobs.groovy
+++ console-scripts/disableTimerTriggeredJobs.groovy
@@ -8,7 +8,7 @@ import hudson.model.*
 import hudson.triggers.*
 import com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger
 
-def items = Jenkins.instance.getItems()
+def items = Jenkins.instance.getAllItems()
 items.each { item ->
     if(item.hasProperty('triggers')){
         item.triggers.values().each { trigger ->
